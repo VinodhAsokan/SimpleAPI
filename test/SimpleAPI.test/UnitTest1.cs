20bbc4edc7c27efed6bd43e54ed0f87c09059ce1
Vinodh.A <Vinodh.A@HTSS.COM>
2021-10-03 10:38:03 +0530

    Initial Commit

---
+++ test/SimpleAPI.test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.test
+{
+    public class UnitTest1
+    {
+        static ILogger<WeatherForecastController> logger = null;
+        WeatherForecastController cnlrWeather = new WeatherForecastController(logger);
+        
+
+        [Fact]
+        public void GetReturnsId()
+        {
+            var ret = cnlrWeather.Get(13);
+            Assert.Equal("13", ret);
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
